1a60741c88fa7bb9b98e409aacd8439f4e3e7085
Alan Kralemann <akralemann@gmail.com>
2021-10-12 08:50:05 -0300

    Se creo heroes routing module, router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppRoutingModule } from './app-routing.module';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
@@ -11,6 +13,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
     ErrorPageComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule
   ],
